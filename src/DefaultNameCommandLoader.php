a4850bd14de21750c82fc1334dd26b1865070289
Bruce Phillips <5489+brucep@users.noreply.github.com>
2020-02-01 09:29:09 -0500

    Add empty array as default value

--- src/DefaultNameCommandLoader.php
+++ src/DefaultNameCommandLoader.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Exception\CommandNotFoundException;
 
 final class DefaultNameCommandLoader implements CommandLoaderInterface
 {
-    private $commands;
+    private $commands = [];
 
     public function __construct(array $classes)
     {
